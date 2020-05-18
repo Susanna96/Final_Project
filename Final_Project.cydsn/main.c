73029e6b9e4c756ac7c13ce2ad9883c542d6bfbc
martinapalmieri <martina.palmieri@mail.polimi.it>
2020-05-18 20:53:40 +0200

    Setting LIS3DH registers (also FIFO buffer) + I2C source and header files

--- Final_Project.cydsn/main.c
+++ Final_Project.cydsn/main.c
@@ -1,26 +1,244 @@
 /* ========================================
+ * \file main.c
  *
- * Copyright YOUR COMPANY, THE YEAR
- * All Rights Reserved
- * UNPUBLISHED, LICENSED SOFTWARE.
- *
- * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
  *
  * ========================================
 */
+
+// Include header files
+#include "I2C_Interface.h"
 #include "project.h"
+#include "stdio.h"
+
+//Brief slave device address
+#define LIS3DH_DEVICE_ADDRESS 0x18
+
+//Brief WHO AM I register address
+#define LIS3DH_WHO_AM_I_REG_ADDR 0x0F
+
+//Brieg CONTROL REGISTER 1 address
+#define LIS3DH_CTRL_REG1 0x20
+
+/*Brief HEX value for CONTROL REGISTER 1: High resolution mode at 100 Hz
+CTRL_REG1[3]=LPen=0 (HR mode);
+CTRL_REG1[7:4]=ODR[3:0]=0101 (100 Hz)*/
+#define LIS3DH_HR_MODE_100_HZ_CTRL_REG1 0x57
+
+//Brief CONTROL REGISTER 4 address
+#define LIS3DH_CTRL_REG4 0x23
+
+/*Brief HEX value for CONTROL REGISTER 4: +- 4.0 g FSR
+CTRL_REG4[3]=1 (High resolution enabled)
+CTRL_REG4[5:4]=FS[1:0]=01 (4.0 g FSR) */
+#define LIS3DH_CTRL_REG4_2g 0x98
+
+//Brief CONTROL REGISTER 5 address
+#define LIS3DH_CTRL_REG5 0x24
+
+/*Brief HEX value for CONTROL REGISTER 5:
+FIFO_EN=1 (enable FIFO buffer)*/
+#define LIS3DH_FIFO_ENABLED 0x40
+
+//Brief FIFO CONTROL REGISTER address
+#define LIS3DH_FIFO_CTRL_REG 0x2E
+
+/*Brief HEX value for FIFO CONTROL REGISTER:
+FM[1:0]=01 (FIFO mode)
+TR=1 (Triggering signal on INT2)*/
+#define LIS3DH_FIFO_MODE 0x60
+
+//Brief INT2 CONFIGURATION register address
+#define LIS3DH_INT2_CFG 0x34
+
+/*Brief HEX value for IN2 CONFIGURATION register:
+ZHIE=YHIE=ZHIE=1 (Interrupt request on measured accel. value lower than preset
+threshold)
+*/
+#define LIS3DH_INT2_HIGH_EVENT 0x2A
+
+//Brief INT2 THRESHOLD register address
+#define LIS3DH_INT2_THS 0x36
+
+/*Brief HEX value for INT2 THRESHOLD register:
+
+*/
+#define LIS3DH_THRESHOLD_EVENTS 
+
+//Brief INT2 DURATION register address
+#define LIS3DH_INT2_DURATION 0x37 
+
+/*Brief HEX value for INT2 DURATION register:
+
+*/
+#define LIS3DH_DURATION_EVENT 
+
+//Brief OUT_X_L register address (x-axis output LSB)
+#define LIS3DH_OUT_X_L 0x28
+
+//Brief OUT_Y_L register address (y-axis output LSB)
+#define LIS3DH_OUT_Y_L 0x2A
+
+//Brief OUT_Z_L register address (z-axis output LSB)
+#define LIS3DH_OUT_Z_L 0x2C
+
+//Brief value of sensitivity in High Resolution mode (2 mg/digit)
+#define HR_SENSITIVITY 2;
+
 
 int main(void)
 {
-    CyGlobalIntEnable; /* Enable global interrupts. */
+    CyGlobalIntEnable; 
+    
+    /* INITIALIZATION */
+    
+    I2C_Peripheral_Start();
+    UART_Start();
+    
+  
+    //"The boot procedure is complete about 5 milliseconds after device power-up."
+    CyDelay(5); 
+   
+    /*READING AND WRITING OF LIS3DH REGISTERS*/
+    
+    char message[50];
+    
+    // CONTROL REGISTER 1
+    uint8_t ctrl_reg1; 
+    ErrorCode error = I2C_Peripheral_ReadRegister(LIS3DH_DEVICE_ADDRESS,
+                                        LIS3DH_CTRL_REG1,
+                                        &ctrl_reg1);
+     if (error == NO_ERROR)
+    {
+        sprintf(message, "CONTROL REGISTER 1: 0x%02X\r\n", ctrl_reg1);
+        UART_PutString(message); 
+    }
+    else
+    {
+        UART_PutString("Error occurred during I2C comm to read control register 1\r\n");   
+    }
+    
 
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    if (ctrl_reg1 != LIS3DH_HR_MODE_100_HZ_CTRL_REG1)
+    {
+        ctrl_reg1 = LIS3DH_HR_MODE_100_HZ_CTRL_REG1;
+        error = I2C_Peripheral_WriteRegister(LIS3DH_DEVICE_ADDRESS,
+                                             LIS3DH_CTRL_REG1,
+                                             ctrl_reg1);  
+        
+        if (error == NO_ERROR)
+        {
+            sprintf(message, "CONTROL REGISTER 1 successfully written as: 0x%02X\r\n", ctrl_reg1);
+            UART_PutString(message); 
+        }
+        else
+        {
+            UART_PutString("Error occurred during I2C comm to set control register 1\r\n");   
+        }
+    }
+   
+    //CONTROL REGISTER 4   
+    uint8_t ctrl_reg4;
+    error = I2C_Peripheral_ReadRegister(LIS3DH_DEVICE_ADDRESS,
+                                        LIS3DH_CTRL_REG4,
+                                        &ctrl_reg4);
+     if (error == NO_ERROR)
+    {
+        sprintf(message, "CONTROL REGISTER 4: 0x%02X\r\n", ctrl_reg4);
+        UART_PutString(message); 
+    }
+    else
+    {
+        UART_PutString("Error occurred during I2C comm to read control register 4\r\n");   
+    }
+    
+   
+    if (ctrl_reg4 != LIS3DH_CTRL_REG4_2g)
+    {
+        ctrl_reg4 = LIS3DH_CTRL_REG4_2g;
+        error = I2C_Peripheral_WriteRegister(LIS3DH_DEVICE_ADDRESS,
+                                         LIS3DH_CTRL_REG4,
+                                         ctrl_reg4);
+        if (error == NO_ERROR)
+        {
+            sprintf(message, "CONTROL REGISTER 4 successfully written as: 0x%02X\r\n", ctrl_reg4);
+            UART_PutString(message); 
+        }
+        else
+        {
+            UART_PutString("Error occurred during I2C comm to set control register 4\r\n");   
+        }
+    }
+    
+    //CONTROL REGISTER 5  
+    uint8_t ctrl_reg5;
+    error = I2C_Peripheral_ReadRegister(LIS3DH_DEVICE_ADDRESS,
+                                        LIS3DH_CTRL_REG5,
+                                        &ctrl_reg5); 
+     if (error == NO_ERROR)
+    {
+        sprintf(message, "CONTROL REGISTER 5: 0x%02X\r\n", ctrl_reg5);
+        UART_PutString(message); 
+    }
+    else
+    {
+        UART_PutString("Error occurred during I2C comm to read control register 5\r\n");   
+    }
+    
+    
+    if (ctrl_reg5 != LIS3DH_FIFO_ENABLED)
+    {
+        ctrl_reg5 = LIS3DH_FIFO_ENABLED;
+        error = I2C_Peripheral_WriteRegister(LIS3DH_DEVICE_ADDRESS,
+                                         LIS3DH_CTRL_REG5,
+                                         ctrl_reg5);
+        if (error == NO_ERROR)
+        {
+            sprintf(message, "CONTROL REGISTER 5 successfully written as: 0x%02X\r\n", ctrl_reg5);
+            UART_PutString(message); 
+        }
+        else
+        {
+            UART_PutString("Error occurred during I2C comm to set control register 5\r\n");   
+        }
+    }
+    
+    //FIFO CONTROL REGISTER
+    uint8_t fifo_ctrl_reg;
+    error = I2C_Peripheral_ReadRegister(LIS3DH_DEVICE_ADDRESS,
+                                        LIS3DH_FIFO_CTRL_REG,
+                                        &fifo_ctrl_reg); 
+     if (error == NO_ERROR)
+    {
+        sprintf(message, "FIFO CONTROL REGISTER: 0x%02X\r\n", fifo_ctrl_reg);
+        UART_PutString(message); 
+    }
+    else
+    {
+        UART_PutString("Error occurred during I2C comm to read FIFO control register \r\n");   
+    }
+    
+    
+    if (fifo_ctrl_reg != LIS3DH_FIFO_MODE)
+    {
+        fifo_ctrl_reg = LIS3DH_FIFO_MODE;
+        error = I2C_Peripheral_WriteRegister(LIS3DH_DEVICE_ADDRESS,
+                                         LIS3DH_FIFO_CTRL_REG,
+                                         fifo_ctrl_reg);
+        if (error == NO_ERROR)
+        {
+            sprintf(message, "FIFO CONTROL REGISTER successfully written as: 0x%02X\r\n", fifo_ctrl_reg);
+            UART_PutString(message); 
+        }
+        else
+        {
+            UART_PutString("Error occurred during I2C comm to set FIFO control register \r\n");   
+        }
+    }
+    
 
     for(;;)
     {
-        /* Place your application code here. */
+     
     }
 }
-
 /* [] END OF FILE */
